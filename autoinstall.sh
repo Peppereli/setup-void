a8ffe07bf42612e9b3bc7f5a648ab1cec4f79379
Peeps <lastf8474@gmail.com>
2025-08-06 23:48:52 +0500

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -7,8 +7,7 @@ sudo xbps-install -S void-repo-nonfree void-repo-multilib-nonfree
 sudo xbps-install -Syu
 
 echo "INSTALLING PACKAGES..."
-sudo xbps-install -S sway swaylock swayidle swaybg alacritty Waybar wmenu font-awesome fastfetch rofi curl libreoffice NetworkManager network-manager-applet iwd bleachbit pavucontrol gvfs gvfs-mtp nwg-look dunst polkit-gnome btop gcc zsh sxiv zip unzip 7zip tar Thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler ffmpegthumbnailer mpv xarchiver geany cmus upower qbittorrent grim slurp neovim xorg-server-xwayland xdg-desktop-portal-wlr zathura zathura-cb zathura-pdf-poppler noto-fonts-ttf noto-fonts-emoji elogind dbus polkit steam unrar light mesa pulseaudio pulseaudio-utils libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit 
-
+sudo xbps-install -S sway swaylock swayidle swaybg alacritty Waybar wmenu font-awesome fastfetch rofi curl libreoffice NetworkManager network-manager-applet iwd bleachbit pavucontrol gvfs gvfs-mtp nwg-look dunst polkit-gnome btop gcc zsh sxiv zip unzip 7zip tar Thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler ffmpegthumbnailer mpv xarchiver geany cmus upower qbittorrent grim slurp neovim xorg-server-xwayland xdg-desktop-portal-wlr zathura zathura-cb zathura-pdf-poppler noto-fonts-ttf noto-fonts-emoji elogind dbus polkit steam unrar light mesa pulseaudio pulseaudio-utils
 
 #sudo xbps-install -S pam-devel xcb-util-xrm libxcb-devel
 
