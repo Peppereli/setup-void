9e7d7714c43acaba66382528511e2c9f233c33b1
Peeps <lastf8474@gmail.com>
2025-08-11 04:53:09 +0000

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -9,14 +9,11 @@ sudo xbps-install -Syu
 echo "INSTALLING PACKAGES..."
 sudo xbps-install -S sway swaylock swayidle swaybg alacritty Waybar font-awesome fastfetch rofi curl libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-gnome flatpak NetworkManager network-manager-applet iwd pavucontrol gvfs gvfs-mtp nwg-look dunst polkit-gnome btop gcc qimgv zip unzip 7zip tar Thunar thunar-archive-plugin thunar-volman tumbler ffmpegthumbnailer ffmpeg mpv xarchiver geany cmus upower acpi qbittorrent grim slurp neovim xorg-server-xwayland xdg-desktop-portal-wlr zathura zathura-cb zathura-pdf-poppler elogind dbus polkit steam unrar brillo mesa pulseaudio pulseaudio-utils yazi fzf opendoas mesa-dri-32bit vulkan-loader mesa-vulkan-radeon mesa-vaapi mesa-vdpau mesa-vulkan-radeon-32bit vulkan-loader-32bit groff bc yt-dlp make xhost liberation-fonts-ttf google-fonts-ttf
 
-#sudo xbps-install -S pam-devel xcb-util-xrm libxcb-devel
 echo "CLONING DOTFILES..."
 cd
 mkdir -p ~/Pictures
 git clone https://github.com/Peppereli/dotfiles-void
 cd ~/dotfiles-void
-
-echo "COPYING DOTFILES..."
 mkdir -p ~/.config
 cp -rf config/* ~/.config/
 cp .gtkrc-2.0 ~/
@@ -33,7 +30,6 @@ rm -rf ~/dotfiles-void
 echo "CLONING FONTS..."
 git clone https://github.com/Peppereli/fonts
 cd
-echo "COPYING FONTS..."
 sudo cp -rf ~/fonts/* /usr/share/fonts/
 
 echo "UPDATING FONT CACHE..."
@@ -43,17 +39,13 @@ echo "CLEANING FONTS CLONE..."
 rm -rf ~/fonts
 
 echo "CLONING NVCHAD..."
-git clone -b v2.0 https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+git clone https://github.com/NvChad/starter ~/.config/nvim
 echo "TO INSTALL NVCHAD RUN 'nvim' AND LET IT INSTALL THE PLUGINS"
 
 echo "CLONING GTK THEMES..."
 git clone https://github.com/Peppereli/themes
 cd ~/themes
-
-echo "EXTRACTING GTK THEMES..."
 7z x themes.7z
-
-echo "COPYING GTK THEMES..."
 sudo cp -rf ~/themes/themes/* /usr/share/themes/
 sudo cp -rf ~/themes/icons/* /usr/share/icons/
 cd
@@ -90,10 +82,10 @@ xdg-mime default org.pwmt.zathura.desktop application/x-cbr
 
 xdg-mime default Thunar.desktop inode/directory
 
-xdg-mime default com.brave.Browser.desktop x-scheme-handler/http
-xdg-mime default com.brave.Browser.desktop x-scheme-handler/https
-xdg-mime default com.brave.Browser.desktop text/html
-xdg-mime default com.brave.Browser.desktop application/xhtml+xml
+xdg-mime default librewolf.desktop x-scheme-handler/http
+xdg-mime default librewolf.desktop x-scheme-handler/https
+xdg-mime default librewolf.desktop text/html
+xdg-mime default librewolf.desktop application/xhtml+xml
 
 xdg-mime default mpv.desktop video/mpeg
 xdg-mime default mpv.desktop video/mp4
@@ -146,3 +138,5 @@ sudo ln -s /etc/sv/iwd /var/service
 #flatpak override --user --filesystem=~/Downloads com.brave.Browser
 
 echo "INSTALLATION FINISHED! TIME TO REBOOT. RUN 'sudo reboot'."
+
+sudo reboot
